fe7cf57add8ed25f68569f9fd617cde83fef6d33
Chira Tudor <Chira Tudor>
2016-04-09 19:58:16 +0300

    Add default vis

--- js/app.js
+++ js/app.js
@@ -9,18 +9,27 @@ define([
     var initialize = function () {
         // Pass in our Router module and call it's initialize function
         Router.initialize();
+
+        var visOptions = {
+            zoom: 12,
+            scrollwheel: true,
+            search : false,
+            share: false,
+            center:[48.1441,17.1462]
+        };
+        cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3beee460-fe5a-11e5-b0a3-0e674067d321/viz.json',visOptions);
         window.onload = function () {
             $('#fun-but').on('click', function () {
                 $('#map').empty();
-                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3beee460-fe5a-11e5-b0a3-0e674067d321/viz.json');
+                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3beee460-fe5a-11e5-b0a3-0e674067d321/viz.json',visOptions);
             });
             $('#family-but').on('click', function () {
                 $('#map').empty();
-                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3beee460-fe5a-11e5-b0a3-0e674067d321/viz.json');
+                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3beee460-fe5a-11e5-b0a3-0e674067d321/viz.json',visOptions);
             });
             $('#parking-but').on('click', function () {
                 $('#map').empty();
-                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3c4307ac-fe64-11e5-a29a-0ecd1babdde5/viz.json');
+                cartodb.createVis('map', 'https://blankearth.cartodb.com/api/v2/viz/3c4307ac-fe64-11e5-a29a-0ecd1babdde5/viz.json',visOptions);
             });
 
         }
